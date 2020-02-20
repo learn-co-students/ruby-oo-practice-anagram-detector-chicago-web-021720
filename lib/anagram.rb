d83b359f575d0ee93eca0c997406451267961a55
djoverbeatz29 <jroverby92@gmail.com>
2020-02-20 10:18:09 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :match
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.find_all { |w| w.chars.sort == @word.chars.sort }
+    end
+
+end
