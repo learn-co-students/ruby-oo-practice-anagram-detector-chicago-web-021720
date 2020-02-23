957d47252e1c3b83b9a4112082c622a6127749d0
Ethan Davidson <theejdavidson@gmail.com>
2020-02-23 13:05:23 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def word_arr(word)
+        # helper method for splitting a single word
+        # into a sorted array
+        word.split("").sort
+    end
+
+    def match(matches)
+        target = word_arr(self.word)
+        # we can compare each potential matching word
+        # with the target by sorting them the same way
+        matches.select do |match|
+            word_arr(match) == target
+        end
+    end
+end
+
+edcba = Anagram.new("edcba")
+#edcba.word_arr(edcba.word)
+puts edcba.match(%w(adsdfsd abcde bcdea elf))
