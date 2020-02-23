baac5c03bb590ac5c447cea35f2e92dc37597431
owenabbott <ocabbott@gmail.com>
2020-02-23 13:50:36 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name, :anagram
+    def initialize(name)
+        @name=name
+    end
+
+    def match(array)
+        name_array=name.split("")
+        array_of_anagrams=[]
+        solution=[]
+        for i in array
+            array_of_anagrams.push(i.split(""))
+        end
+        for i in array_of_anagrams
+            if name_array.sort==i.sort
+                 solution.push(array[array_of_anagrams.index(i)])
+            end
+        end
+        solution
+    end
+end
